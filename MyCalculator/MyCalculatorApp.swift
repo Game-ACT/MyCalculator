8932cd2660335a8aee69139a029975b47a4e00f6
noigamegun <76461912+noigamegun@users.noreply.github.com>
2024-07-30 12:46:26 +0700

    Initial Commit

---
+++ MyCalculator/MyCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyCalculatorApp.swift
+//  MyCalculator
+//
+//  Created by Thapat Auechaikasem on 30/7/67.
+//
+
+import SwiftUI
+
+@main
+struct MyCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
